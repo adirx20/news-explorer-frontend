f17779e8b8a72c9d884e76a9bc20e335a350356d
adirx20 <87542990+adirx20@users.noreply.github.com>
2022-10-31 08:41:29 +0200

    add react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
+import Main from '../Main/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header>
+        <Navigation />
+      </Header>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Main />
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function Main() {
     return (
-        <>
-        </>
+        <section className='main'>
+            <div className='main__form-container'>
+                <h1 className='main__title'>What's going on in the world?</h1>
+                <p className='main__description'>Find the latest news on any topic and save them in your personal account.</p>
+                {/* search form component */}
+            </div>
+            {/* news card list component with the loop to create cards from new card component */}
+            {/* about component */}
+        </section>
     );
 };
 
