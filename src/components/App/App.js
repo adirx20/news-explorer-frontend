46ecae169aa0b98506b89e2022afc9ad3efa6409
Adir Sason <adirx20@gmail.com>
2022-12-27 16:47:43 +0200

    add background image and CurrentUserContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
 import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
 import Main from '../Main/Main';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app app__background-image'>
       <Header>
         <Navigation />
       </Header>
