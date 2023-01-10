972b692b80e3722a5af62554923a28004d93e023
adirx20 <87542990+adirx20@users.noreply.github.com>
2023-01-10 02:59:48 +0200

    styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './About.css';
+import avatarImage from '../../images/about-avatar-example.png';
 
 function About() {
     return (
         <section className='about'>
-            <img className='about__image' />
+            <img className='about__image' src={avatarImage} />
             <article className='about__text-container'>
                 <h2 className='about__title'>About the author</h2>
                 <p className='about__text'>
