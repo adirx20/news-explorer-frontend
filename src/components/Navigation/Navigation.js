edf12d8c817068ec3abac084e7a830dd19292f3f
adirx20 <87542990+adirx20@users.noreply.github.com>
2023-03-15 02:27:54 +0200

    navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import './Navigation.css';
 
 function Navigation() {
+    const navigate = useNavigate();
+
+    function navigateToMain() {
+        navigate('/');
+        // onClose
+    }
+
+    function navigateToSavedNews() {
+        navigate('/saved-news');
+        // onClose
+    }
+
     return (
         <nav className='navigation'>
             <button className='navigation__menu-button'></button>
